a57bf4fedc8471a4b79b2d4c5ac77aa359d87d7e
Saran Pariyar <spariyar05@gmail.com>
2024-01-23 11:02:58 +0545

    All vanilla js completed, next part react

--- app/typescript/page.tsx
+++ app/typescript/page.tsx
@@ -12,8 +12,8 @@ arrayWithStringAndNumber = [1, "Hello"]
 // FUNCTION THAT TAKES NUMBER ARGUMENT, AND WILL RETURN NUMBER
   function multiplyNum( num: number ):number{ return num * 2 }
 
-//// INTERFACES
-// Extend
+// Interfaces are just advanced version of type. we can use extend feature in interface too
+
 interface IUser {
   username: string;
   email: string;
@@ -36,6 +36,46 @@ const client: IUser = {
   email: "tom@gmail.com",
   age: 43,
 };
+
+// GENERICS 
+// we generally use <T> for generics but we can use anything. Now it will accept any kind of array
+interface IPostBetter<T> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+//this is using IPostBetter interface which has Generics
+//now it is accepting string array too.
+const testMe: IPostBetter<string> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: ["str", "str2"],
+};
+
+
+// now we can accept any object here
+interface IPostEvenBetter<T extends object> {
+  id: number;
+  title: string;
+  desc: string;
+  extra: T[];
+}
+
+// we specified the id inside object should be number here now
+const testMe2: IPostEvenBetter<{ id:number }> = {
+  id: 1,
+  title: "post title",
+  desc: "post desc",
+  extra: [{ id: 1 }],
+};
+
+
+
+
+
 
 
 
