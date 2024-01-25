93ba9dbcfdb94cf897d92d54b9eeaf2c58ebab4d
Saran Pariyar <spariyar05@gmail.com>
2024-01-25 15:28:09 +0545

    Testing RTK with blog steps

---
+++ app/RTK/redux/features/auth-slices.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type InitialState = {
+value: AuthState
+}
+
+type AuthState ={
+    isAuth : boolean,
+    username: string,
+    uid: string,
+    isModerator: boolean
+}
+
+const initialState = {
+    value:{
+        isAuth : false,
+        username:"",
+        uid:"",
+        isModerator:false
+
+    }
+} as InitialState
+
+export const auth = createSlice({
+name: "auth",
+initialState,
+reducers:{
+    logOut: () =>{
+        return initialState;
+    }
+
+}
+
+})
